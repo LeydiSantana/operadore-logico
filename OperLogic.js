1a4c2758766cbde0bdd4338649c245d1fbd95799
LeydiSantana <113629316+LeydiSantana@users.noreply.github.com>
2023-01-31 08:22:11 -0400

    Add files via upload

---
+++ OperLogic.js
@@ -0,0 +1,14 @@
+const aSmile = false;
+const bSmile = false;
+
+const alert = "Verdadero, ¡estamos en problemas!";
+
+if(aSmile == false && bSmile == false|| aSmile == true && bSmile == true)
+{
+    console.log(alert)
+}
+
+else
+{
+    console.log("Falso, no estamos en problemas");
+}
